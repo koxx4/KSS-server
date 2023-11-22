0f4e248151846a937cd56b092dc97098408057cc
koxx4 <piotr815@protonmail.com>
2023-11-22 07:23:46 +0100

    formatting

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,28 @@
 mod controllers;
-mod services;
-mod repositories;
 mod models;
+mod repositories;
+mod services;
 
-use mongodb::Client;
-use actix_cors::Cors;
-use actix_web::{App, HttpServer, web};
-use actix_web::middleware::Logger;
-use actix_web::web::Data;
-use env_logger::Env;
 use crate::controllers::events_controller::{get_event_image, get_events, get_unread_events_count};
 use crate::controllers::events_ws_controller::ws_check_new;
 use crate::controllers::health_controller::health_check;
-
+use actix_cors::Cors;
+use actix_web::middleware::Logger;
+use actix_web::web::Data;
+use actix_web::{web, App, HttpServer};
+use env_logger::Env;
+use mongodb::Client;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-
     std::env::set_var("RUST_LOG", "debug");
     env_logger::init_from_env(Env::default().default_filter_or("info"));
 
-    let client = Client::with_uri_str("mongodb://localhost:27017/?replicaSet=rs0").await.unwrap();
+    let client = Client::with_uri_str("mongodb://localhost:27017/?replicaSet=rs0")
+        .await
+        .unwrap();
 
     HttpServer::new(move || {
-
         let cors = Cors::default()
             .allow_any_origin() // Umożliwia żądania z dowolnego źródła
             .allow_any_method() // Umożliwia wszystkie metody HTTP
@@ -40,7 +39,7 @@ async fn main() -> std::io::Result<()> {
             .service(get_event_image)
             .route("api/kss/ws/check-new", web::get().to(ws_check_new))
     })
-        .bind(("0.0.0.0", 8080))?
-        .run()
-        .await
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
 }
